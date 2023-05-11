234e197de453e549c7925cf55a34782f1dd95909
carlhernikrolf <henrik.aaslund@gmail.com>
2023-05-11 18:04:44 +0100

    experimental results

--- configs/more_simple_ablations.py
+++ configs/more_simple_ablations.py
@@ -1,7 +1,7 @@
 from .envs import cells3easier_env
 from agents import PeUcrlAgt, NoPruningAgt, NoShieldAgt, NoPruningNoShieldAgt
 
-repeats = 5
+repeats = 20
 
 config = {
     'env': [
@@ -18,10 +18,10 @@ config = {
     ] * 8 * repeats,
     'regulatory_constraints': [
         *['P>=1 [ G n<=2 ] & P>=1 [ G n_children<=0]'] * 4,
-        *['P>=1 [ G n<=2 ] & P>=1 [ G n_children<=0] P>=0.80 [ F<=20 n<=1 ]'] * 4,
+        *['P>=1 [ G n<=2 ] & P>=1 [ G n_children<=0] & P>=0.80 [ F<=20 n<=1 ]'] * 4,
     ] * repeats,
     'max_n_time_steps': [
-        30
+        40
     ] * 8 * repeats,
     'dir': [str(i) + '/' for i in range(8 * repeats)],
     'super_dir': 'more_simple_ablations/',
