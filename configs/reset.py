9b6cf8bd967bc25fd4bfc91a23d7bf24209a703a
carlhernikrolf <henrik.aaslund@gmail.com>
2023-07-07 16:21:17 +0100

    installation fixes

--- configs/reset.py
+++ configs/reset.py
@@ -4,7 +4,7 @@ from agents import PeUcrlAgt, UnsafeBaselineAgt
 # sota
 from agents import AlwaysSafeAgtPsoAgt, AupAgt, NationLikeAgt
 
-n_repeats = 3
+n_repeats = 25
 n_agts = 5
 
 config = {
@@ -19,9 +19,7 @@ config = {
     'seed': [i for i in range(n_agts * n_repeats)],
     'regulatory_constraints': [
         {'prism_props': 'P>=0.5 [ X X n<=1 ]'},
-        {'prism_props': 'P>=0.5 [ X X n<=1 ]'},
-        {'prism_props': 'P>=0.5 [ X X n<=1 ]'},
-        {'prism_props': 'P>=0.5 [ X X n<=1 ]'},
+        {'prism_props': 'N/A'},
         {'delicate_cell_classes': ['children']},
         {'regularization_param': 1., 'n_aux_reward_funcs': 10},
         {'conservativeness': 0.2, 'update_frequency': 50},
